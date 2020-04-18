1686982adbf68ff6c2a84f870fbe662147908239
Charlie Payne <charliepayne1101@gmail.com>
2020-04-18 12:34:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? > do |element|
+  array.select do |element|
     if element.length > 4
+      return 
   end
 end
